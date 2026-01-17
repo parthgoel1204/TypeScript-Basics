ba6c371f9bffd42e032791eeb7b977f14c2ed4a4
Parth Goel <parthgoel1204@gmail.com>
2026-01-17 19:18:47 +0530

    Initial Commit: What is Ts & why to learn it

---
+++ file.ts
@@ -0,0 +1,13 @@
+// Typescript is often called the "SUPERSET" of JavaScript.
+
+// It allows the developers to write JS in a precise manner so that the code faces a lot less error in the runtime.
+// TYpeScript all about type safety 
+// In the JS we can add 2 + '2' = "22";
+// The above was possible because "TYPESAFETY" was missing .
+
+// TypeScript does "STATIC CHECKING" which means the parser of the language ot the entire syntax of the language is constantly being analyzed by the IDE'S
+
+// We write our code in TS and that is further transpiled into JS.
+// TypeScript is just a development tool and our whole code that we sent to the production is 100% JS.
+// It helps to write better code with less problems with a more scalable and maintainable code that is easily understandable and the chances of it producing errors are very less.
+// It's more of a wrapper around JS. 
