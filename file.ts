c7892400f89dab089f17d3013587cd353f1e0e7e
Parth Goel <parthgoel1204@gmail.com>
2026-01-18 22:45:44 +0530

    feat : type aliases , readonly & optional in TS

--- file.ts
+++ file.ts
@@ -112,4 +112,48 @@ createUser(newUsr); // weird behaviour of objects because if we were to pass ema
 function createCourse() : {name:string , price: number}{
     return {name :"react.js" , price : 399};
 }
+
+// Type Aliases
+
+// type User = {
+//     name: string;
+//     email : string;
+//     isActive : boolean;
+// }
+
+// function createNewUser (user : User):User{
+// // function body
+//     return {name: "" , email: "", isActive: true};
+// }
+
+// createNewUser({name: "" , email: "", isActive: true});
+
+type User = {
+    readonly _id : string;
+    name: string;
+    email : string;
+    isActive : boolean;
+    creditDetails? : number
+}
+let myUser: User = {
+    _id : "1234",
+    name : "p",
+    email : "palplk",
+    isActive : false
+}
+
+myUser.email = "ncvsnvjnv";
+// myUser._id = "bccvb"; this is not allowed due to the readonly 
+
+type cardnumber = {
+    cardnumber : string;
+}
+
+type cardDate = {
+    carddate : string;
+}
+
+type cardDetails = cardDate & cardnumber & {
+    cvv : number;
+}
 export {}
