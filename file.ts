7c235d21c31f4d3a36947878e7a0639d400d7a6a
Parth Goel <parthgoel1204@gmail.com>
2026-01-18 20:45:10 +0530

    feat :  various types , functions & objects in TS

--- file.ts
+++ file.ts
@@ -10,4 +10,106 @@
 // We write our code in TS and that is further transpiled into JS.
 // TypeScript is just a development tool and our whole code that we sent to the production is 100% JS.
 // It helps to write better code with less problems with a more scalable and maintainable code that is easily understandable and the chances of it producing errors are very less.
-// It's more of a wrapper around JS. 
+// It's more of a wrapper around JS.
+
+// let user = {name:"Hitesh" , age:18};
+
+// console.log(user);
+console.log("Parth");
+
+// We write a lot of code in Typescript but it is a safer code 
+
+let greetings:string = "Hello Parth";
+let myNUm = 6;
+
+console.log(greetings);
+
+// we don't specifically declare Interger or Float we simply call all of them as NUMBER 
+// number
+let userId:number = 334466;
+// now whenever we qaccess userID using dot(.) it will only give methods that can be applied on number
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// any
+
+// let hero;
+let hero : string;
+
+function getHero(){
+    return "THOR"
+}
+hero = getHero(); // hero is getting any type that is any type can be returned and stored in it.
+// So, for correcting this we will use ':' and tell the type explicitly at the declaration time.
+// let hero : string;
+// "any" is not a typechecker it turns off the typechecker
+
+/* ## noimplicitAny
+When you don’t specify a type, and TypeScript can’t infer it from context, the compiler will typically default to any.
+
+You usually want to avoid this, though, because any isn’t type-checked. Use the compiler flag noImplicitAny to flag any implicit any as an error.
+*/
+
+// Function 
+function addTwo(num: number){
+    return num + 2;
+}
+
+addTwo(5);
+
+function getUpper(val:string){
+    return val.toUpperCase();
+}
+
+getUpper("hi");
+
+function signUp(name:string , email:string, isPaid: boolean){}
+signUp( "abyu" , "cnjsbjb" , false);
+
+let login = (name:string , email:string, isPaid: boolean = false)=>{}
+login("Parth" , "parth@google.com");
+
+function add(num : number):number{ // the colon outside the function tells the function to allow only number to return 
+    return num + 10;
+}
+
+const getHello = (s:string):string => {
+    return "";
+}
+
+const heros = ["thor" ,"spiderman", "dr. strange"]
+
+heros.map( (hero):string =>{
+    return `hero is ${hero}`;
+});
+
+function consoleError(errmssg: string):void{ //void specifies this function doesn't returns anything
+    console.log(errmssg);
+}
+
+// Some functions never return a value
+// It leads to forceful termination of the program.
+function handleError(errmssg: string):never{
+    throw new Error(errmssg)
+}
+
+// Objects
+const User = {
+    name: "Parth",
+    email : "parth@google.com",
+    isActive : true     
+}
+
+function createUser({ name, isPaid }: { name: string; isPaid: boolean }) {
+  // function body
+}
+
+// createUser({ name: "Parth", isPaid: false });
+let newUsr = { name: "Parth", isPaid: false , email: "parth@google.com"};
+createUser(newUsr); // weird behaviour of objects because if we were to pass email as argument in create user that was not allowed but now we have passed using a different method 
+
+function createCourse() : {name:string , price: number}{
+    return {name :"react.js" , price : 399};
+}
+export {}
