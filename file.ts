0d62e73f3df996fb7eb964a53e1cd9d8d918190d
Parth Goel <parthgoel1204@gmail.com>
2026-01-22 00:28:43 +0530

    feat : narrowing , instanceof narrowing , type predicates, discriminated unions and exhaustive type cheking

--- file.ts
+++ file.ts
@@ -430,4 +430,99 @@ class Sellable <T> {
         this.cart.push(product)
     }
 }
+
+// Detections : Cases about which we need to more catious
+function detectType(val :string | number){
+    if(typeof val === "string"){
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+
+function provideId(id :string | null){
+    if(!id){
+        console.log("Please provide ID!");
+        return ;
+    }
+    // id ID is available perform operations on it 
+}
+
+// ##typeof are also called type guards which are used for strict type checking 
+
+// instanceof narrowing 
+
+// typeof checks for the default type and instanceof checks whether this object was an instance of some class 
+
+function logValue(x :Date | string){
+    if(x instanceof Date){
+        console.log(x.toUTCString());
+    }
+    else{
+        console.log(x.toUpperCase());
+    }
+}
+
+
+// type predicates 
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet : Fish |Bird): pet is Fish{
+    return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish| Bird){
+    if(isFish(pet)){
+        pet ;
+        return "fish food";
+    }
+    else{
+        pet;
+        return "Bird food";
+    }
+}
+
+// Discriminated UNions : It tells us to use kind to identify the type of interface 
+
+interface Circle{
+    kind : "circle",
+    radius : number
+}
+
+interface Square{
+    kind : "square",
+    side : number
+}
+
+interface Rectangle{
+    kind : "rectangle",
+    width : number,
+    length : number
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape : Shape){
+    if(shape.kind === "circle"){
+        return Math.PI * shape.radius **2;
+    }
+
+    // return shape.side * shape.side;
+}
+
+// Never Type & Exhaustive Checking
+
+function getArea(shape: Shape){
+    switch(shape.kind){
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultForShape: never = shape
+            return _defaultForShape;
+    }
+}
 export {}
