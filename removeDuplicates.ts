66deb9a34e540a2aac77f66c21e43d0203080b72
Parth Goel <parthgoel1204@gmail.com>
2026-01-22 23:03:01 +0530

    feat : practice question

---
+++ removeDuplicates.ts
@@ -0,0 +1,13 @@
+function removeDuplicates<T>(arr: T[]): T[] {
+  const result: T[] = [];
+
+  for (const item of arr) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+export default removeDuplicates;
